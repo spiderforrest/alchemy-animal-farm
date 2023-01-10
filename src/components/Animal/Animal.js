540f44629d21dbb9db2a5214f8d0e7230bebdbcb
spiderforrest <spider@spood.org>
2023-01-09 16:00:58 -0800

    images

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,7 +4,7 @@ export default function Animal(arg) {
   return (
     <div className="animal" style={{ top: `${arg.top}`, left: `${arg.left}` }}>
       <h3 className="name">{arg.name}</h3>
-      <img className="name" src={`../../../public/animals/${arg.type}.svg`}></img>
+      <img className="name" src={`./animals/${arg.type}.svg`}></img>
       <p className="text">{arg.says}</p>
     </div>
   );
